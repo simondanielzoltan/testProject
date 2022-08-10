f01fd6ee567576316045d92dad249fb58683575d
Simon Dániel Zoltán <badsimon@hotmail.hu>
2022-08-10 19:28:23 +0200

    refactor db config

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
 <?php
   class Db {
     private static $instance = null;
+    private static $hostname = 'localhost';
+    private static $userName = 'root';
+    private static $userPassword = 'rootpasswordgiven';
+    private static $databaseName = 'test';
+    private static $port = 3600;
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = mysqli_connect("localhost", "root", "rootpasswordgiven", "test",3600);
+            self::$instance = mysqli_connect(self::$hostname, self::$userName, self::$userPassword, self::$databaseName,self::$port);
         }
         return self::$instance;
     }
