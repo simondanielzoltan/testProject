0f964077855aa1e75050ed4f452a265476cd95f1
Simon Dániel Zoltán <badsimon@hotmail.hu>
2022-08-10 19:35:07 +0200

    fix typo

--- view/home.php
+++ view/home.php
@@ -3,7 +3,7 @@
 <div class="p-4">
     <h3>Welcome!</h3>
     <p>
-    Please choose from <a href="index.php?page=user">User</a> or <a href="index.php?page=ad">Advertisements</a> options.
+    Please choose from <a href="index.php?page=user">Users</a> or <a href="index.php?page=ad">Advertisements</a> options.
     </p>
 
 </div>
